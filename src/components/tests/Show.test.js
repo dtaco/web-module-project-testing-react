9c2abd199beac478563d3394c818234191ab99a3
David Tacoronte <d.tacoronte89@gmail.com>
2023-10-19 15:53:28 -0400

    finished all tests suites for MVP

--- src/components/tests/Show.test.js
+++ src/components/tests/Show.test.js
@@ -51,6 +51,6 @@ test('component renders when no seasons are selected and when rerenders with a s
     expect(episodes).not.toBeInTheDocument();
 
     rerender(<Show show={testShow} selectedSeason={1}/>);
+    episodes = screen.queryByTestId('episodes-container');
     expect(episodes).toBeInTheDocument();
-
  });
